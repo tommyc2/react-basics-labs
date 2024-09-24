f78e967057116c8d21a3c3180e70468fd98215b8
tommyc2 <20101841@mail.wit.ie>
2024-09-24 12:11:14 +0100

    Extra Lab 2

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import Task from './components/Task';
 
 const App = () => {
+
   return (
       <div className="container">
         <h1>Tasky</h1>
-        <Task title="Dishes" deadline="Today" description="Do the dishes ASAP" />
-        <Task title="Laundry" deadline="Tomorrow" description="Do the laundry tomorrow" />
-        <Task title="Tidy" deadline="Today" description="Tidy my bed today"/>
-        <Task title="Mop" deadline="Tomorrow" description="Mop floor in my room"/>
+        <Task title="Dishes" deadline="Today" description="Do the dishes ASAP" difficulty="3/10"/>
+        <Task title="Laundry" deadline="Tomorrow" description="Do the laundry tomorrow" difficulty="3/10"/>
+        <Task title="Tidy" deadline="Today" description="Tidy my bed today" difficulty="3/10"/>
+        <Task title="Mop" deadline="Tomorrow" description="Mop floor in my room" difficulty="3/10"/>
       </div>
   );
 }
--- tasky/src/components/Task.js
+++ tasky/src/components/Task.js
@@ -1,10 +1,13 @@
+import React, { useState } from 'react';
 
 const Task = (props) => {
+
     return (
         <div className="card">
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p className='description'>{props.description}</p>
+            <p>{props.difficulty}</p>
         </div>
     );
 }
