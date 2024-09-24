37c15c9b5ec031547529d3f4a65644bceef8a577
tommyc2 <20101841@mail.wit.ie>
2024-09-24 11:46:30 +0100

    Complete Tasky Lab 2 with Task component

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Task from './components/Task';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <h1>Tasky</h1>
+        <Task title="Dishes" deadline="Today" description="Do the dishes ASAP" />
+        <Task title="Laundry" deadline="Tomorrow" description="Do the laundry tomorrow" />
+        <Task title="Tidy" deadline="Today" description="Tidy my bed today"/>
+        <Task title="Mop" deadline="Tomorrow" description="Mop floor in my room"/>
+      </div>
   );
 }
 
