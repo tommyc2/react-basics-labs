3944b14bc1781c11b0a1ca73d0f02a4f371b0924
tommyc2 <20101841@mail.wit.ie>
2024-10-01 12:26:30 +0100

    Add task functionality

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Task from './components/Task';
 import React, { useState } from 'react';
 import AddTaskForm from "./components/Form";
+import { v4 as uuidv4 } from 'uuid';
 
 const App = () => {
 
@@ -13,6 +14,33 @@ const App = () => {
         ]
     });
 
+    const [ formState, setFormState ] = useState({
+        title: "",
+        description: "",
+        deadline: ""
+    });
+
+    const formChangeHandler = (event) => {
+        let form = {...formState};
+
+        switch(event.target.name) {
+            case "title":
+                form.title = event.target.value;
+                break;
+            case "description":
+                form.description = event.target.value;
+                break;
+            case "deadline":
+                form.deadline = event.target.value;
+                break;
+            default:
+                form = formState;
+        }
+        setFormState(form);
+    }
+
+    console.log(formState);
+
     const doneHandler = (taskIndex) => {
         const tasks = [...taskState.tasks];
         tasks[taskIndex].done = !tasks[taskIndex].done;
@@ -23,6 +51,18 @@ const App = () => {
     const deleteHandler = (taskIndex) => {
         const tasks = [...taskState.tasks];
         tasks.splice(taskIndex, 1);
+        setTaskState({tasks});
+    }
+
+    const formSubmitHandler = (event) => {
+        event.preventDefault();
+
+        const tasks = [...taskState.tasks];
+        const form = {...formState};
+
+        form.id = uuidv4();
+
+        tasks.push(form);
         setTaskState({tasks});
     }
 
@@ -40,7 +80,7 @@ const App = () => {
                     deleteTask = {() => deleteHandler(index)}
                 />
             ))}
-            <AddTaskForm />
+            <AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />
         </div>
     );
 }
