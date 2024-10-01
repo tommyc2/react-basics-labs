6d67c6a735712e0e5da842a9e3019d9c93c597fd
tommyc2 <20101841@mail.wit.ie>
2024-10-01 12:04:44 +0100

    Add task: form added

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Task from './components/Task';
 import React, { useState } from 'react';
+import AddTaskForm from "./components/Form";
 
 const App = () => {
 
@@ -39,6 +40,7 @@ const App = () => {
                     deleteTask = {() => deleteHandler(index)}
                 />
             ))}
+            <AddTaskForm />
         </div>
     );
 }
